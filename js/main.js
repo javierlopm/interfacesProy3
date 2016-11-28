46b3a901e83c0ce1bf8edce7725e00554b590103
Rebeca Machado G <Rebeca Machado G>
2016-11-27 21:19:21 -0400

    Iconos y location mark

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
  * @author Javier Lopez
  **/
 
- function initGauges(event) {
+function initGauges(event) {
 
   var electricity, water, recycling, co2emissions;
 
@@ -68,10 +68,12 @@
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 6
+    center: {lat: 10.410482, lng: -66.881218},
+    zoom: 17,
+    zoomControl: false,
+    scaleControl: false,
+    streetViewControl: false
   });
-  var infoWindow = new google.maps.InfoWindow({map: map});
 
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
@@ -80,16 +82,100 @@ function initMap() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      
       map.setCenter(pos);
+
+      var myloc = new google.maps.Marker({
+        position: pos,
+        clickable: false,
+        icon: new google.maps.MarkerImage('http://maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+                                          new google.maps.Size(22,22),
+                                          new google.maps.Point(0,18),
+                                          new google.maps.Point(11,11)),
+        shadow: null,
+        zIndex: 999,
+        map: map
+      });    
+
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
   } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
+  } 
+
+  var icons = {
+    glass: {
+      icon: 'http://icons.veryicon.com/png/System/Glyphish/142%20wine%20bottle.png'
+    },
+    paper: {
+      icon: 'http://www.totalshareware.com/ApplicationIcons/68510-1-Icon.gif'
+    },
+    battery: {
+      icon: 'http://findicons.com/files/icons/1588/farm_fresh_web/32/battery.png'
+    }
+  };
+
+  function addMarker(feature) {
+    var marker = new google.maps.Marker({
+      position: feature.position,
+      icon: icons[feature.type].icon,
+      map: map
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      alert("Presionaste un icono");
+    });
+  }
+
+  var features = [
+    {
+      position: new google.maps.LatLng(10.410358, -66.882411),
+      type: 'battery'
+    }, {
+      position: new google.maps.LatLng(10.410104, -66.883641),
+      type: 'battery'
+    }, {
+      position: new google.maps.LatLng(10.411402, -66.881002),
+      type: 'battery'
+    }, {
+      position: new google.maps.LatLng(10.410442, -66.880535),
+      type: 'battery'
+    }, {
+      position: new google.maps.LatLng(10.409698, -66.881721),
+      type: 'paper'
+    }, {
+      position: new google.maps.LatLng(10.409830, -66.883298),
+      type: 'paper'
+    }, {
+      position: new google.maps.LatLng(10.410658, -66.882295),
+      type: 'paper'
+    }, {
+      position: new google.maps.LatLng(10.412046, -66.882102),
+      type: 'paper'
+    }, {
+      position: new google.maps.LatLng(10.410479, -66.880321),
+      type: 'paper'
+    }, {
+      position: new google.maps.LatLng(10.410152, -66.880643),
+      type: 'glass'
+    }, {
+      position: new google.maps.LatLng(10.411059, -66.881689),
+      type: 'glass'
+    }, {
+      position: new google.maps.LatLng(10.409998, -66.882579),
+      type: 'glass'
+    }, {
+      position: new google.maps.LatLng(10.408911, -66.882831),
+      type: 'glass'
+    }, {
+      position: new google.maps.LatLng(10.411544, -66.882488),
+      type: 'glass'
+    }
+  ];
+
+  for (var i = 0, feature; feature = features[i]; i++) {
+    addMarker(feature);
   }
 }
 
